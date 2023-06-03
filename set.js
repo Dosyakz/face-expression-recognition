474b2a8c7532f915423f062e3c64f6a396957732
aidoskz <aidos_2505@inbox.ru>
2023-06-03 20:49:45 +0800

    show in the span how many second changed

--- set.js
+++ set.js
@@ -1,10 +1,19 @@
 function getVal(){
     const myInput = document.querySelector('#vv');    
     number_sec = myInput.value;
-    if(number_sec>9){
-        alert("Number should be less than 10.Please try again!");
-    }
-    else if(number_sec<9){
-        alert(number_sec);
-    }
+    if(isNaN(number_sec)){
+        alert("please this is not number");
+     }else{
+        if(number_sec>9){
+            alert("Number should be less than 10.Please try again!");
+        }
+        else if(number_sec<9){
+            console.log(number_sec);
+            let div = document.querySelector('#changed');
+            div.innerText = "изменен на "+ number_sec;
+        }
+     }
+    
+    
 }
+
